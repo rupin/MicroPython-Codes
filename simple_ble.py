7cd80c5bc684f9fa0badda543171b432fa4a3673
Rupin Chheda <rupin.chheda@gmail.com>
2026-03-09 14:50:06 +0530

    Enhance BLE library for two-way communication
    Added functionality for two-way communication with a buffer for incoming messages and a write interrupt handler.

--- simple_ble.py
+++ simple_ble.py
@@ -1,9 +1,11 @@
-# simple_ble.py - Student Library (Do not modify. Save to ESP32)
+# simple_ble.py - Two-Way Communication Library
 import bluetooth
 from micropython import const
 
 _IRQ_CENTRAL_CONNECT = const(1)
 _IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_WRITE = const(3) # <--- Added Write Interrupt
+
 _UART_UUID = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
 _UART_TX = (bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"), const(0x0002) | const(0x0010))
 _UART_RX = (bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"), const(0x0004) | const(0x0008))
@@ -16,19 +18,15 @@ class BLEConnection:
         self._ble.irq(self._irq)
         ((self._tx, self._rx),) = self._ble.gatts_register_services((_UART_SERVICE,))
         self._connections = set()
+        self._rx_buffer = bytearray() # <--- Added Buffer to store incoming messages
         
-        # Build the foolproof advertising payload we know works
         self._payload = bytearray()
-        
-        # 1. Flags (General Discoverable)
-        self._payload.append(0x02) # Length 2
-        self._payload.append(0x01) # Type: Flags
-        self._payload.append(0x06) # Value: General Discoverable
-        
-        # 2. Local Name
+        self._payload.append(0x02)
+        self._payload.append(0x01)
+        self._payload.append(0x06)
         name_bytes = name.encode('utf-8')
-        self._payload.append(len(name_bytes) + 1) # Length + 1 byte for type
-        self._payload.append(0x09) # Type: Complete Local Name
+        self._payload.append(len(name_bytes) + 1)
+        self._payload.append(0x09)
         self._payload.extend(name_bytes)
         
         self._advertise()
@@ -37,28 +35,44 @@ class BLEConnection:
         if event == _IRQ_CENTRAL_CONNECT:
             conn_handle, _, _ = data
             self._connections.add(conn_handle)
-            print("App connected!")
+            
         elif event == _IRQ_CENTRAL_DISCONNECT:
             conn_handle, _, _ = data
             if conn_handle in self._connections:
                 self._connections.remove(conn_handle)
-            print("App disconnected. Advertising again...")
             self._advertise()
-
-
+            
+        elif event == _IRQ_GATTS_WRITE: # <--- App has sent us data!
+            conn_handle, value_handle = data
+            if conn_handle in self._connections and value_handle == self._rx:
+                # Read the data from the Bluetooth hardware and add to buffer
+                self._rx_buffer += self._ble.gatts_read(self._rx)
 
     def is_connected(self):
         return len(self._connections) > 0
 
     def send_array(self, data_list):
-        """Converts [1024, 2048] to "1024,2048\n" and sends via BLE"""
         if not self.is_connected():
             return
-            
         str_data = ",".join(str(item) for item in data_list) + "\n"
         for conn_handle in self._connections:
             self._ble.gatts_notify(conn_handle, self._tx, str_data.encode())
 
+    # --- NEW: Student-facing Read Functions ---
+    def any(self):
+        """Returns True if there is unread data from the phone."""
+        return len(self._rx_buffer) > 0
+
+    def read(self):
+        """Reads the incoming message as a string and clears the buffer."""
+        if not self.any():
+            return ""
+        
+        # Decode the bytes to a string
+        message = self._rx_buffer.decode('utf-8').strip()
+        # Clear the buffer so we don't read it twice
+        self._rx_buffer = bytearray() 
+        return message
+
     def _advertise(self):
         self._ble.gap_advertise(100000, adv_data=self._payload)
-
