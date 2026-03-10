e252bc67f830a89a8d7430bbb6e52b1a062786db
Rupin Chheda <rupin.chheda@gmail.com>
2026-03-10 09:00:02 +0530

    Improve message reading logic in BLEReadWrite.py
    Refactor message reading to check for incoming data before processing. Added safety check to ignore empty messages.

--- BLEReadWrite.py
+++ BLEReadWrite.py
@@ -10,27 +10,20 @@ print("Ready! Scan for 'ESP32_Read_Write'")
 while True:
     if my_bluetooth.is_connected():
         
-        # --- NEW: Check for incoming messages ---
-        message = my_bluetooth.read()
+        # --- FIX: Only read and print IF data has arrived ---
+        if my_bluetooth.any():
+            message = my_bluetooth.read()
 
-        # Split by comma
-        raw_array = message.split(',')
-
-        # Clean out the \x00 character from each item in the list
-        clean_array = [item.split('\x00')[0] for item in raw_array]
-            
-        print("Clean Array:", clean_array)
-            
-            
+            if message: # Extra safety check to ignore empty strings
+                raw_array = message.split(',')
+                clean_array = [item.split('\x00')[0] for item in raw_array]
+                print("Clean Array:", clean_array)
                 
         # --- Existing Send Logic ---
         x_val=random.randint(0,800)
         y_val=random.randint(0,800)
         
-        # Put data in an array (list)
         data_to_send = [x_val, y_val]
-        #data_to_send = [pot_x.read(), pot_y.read()]
         my_bluetooth.send_array(data_to_send)
         
-        time.sleep(0.5) 
-
+    time.sleep(0.5) 
