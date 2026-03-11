2305f54650e13534e6f43d9c3ed704dad194117a
Rupin Chheda <rupin.chheda@gmail.com>
2026-03-11 15:30:58 +0530

    Integrate NeoPixel for color control via BLE
    Added NeoPixel support for RGB color control based on BLE messages.

--- BLEReadWrite.py
+++ BLEReadWrite.py
@@ -2,15 +2,25 @@ from machine import Pin, ADC
 import time
 from simple_ble import BLEConnection
 import random
+from neopixel import NeoPixel
 
-my_bluetooth = BLEConnection("ESP32_Read_Write")
+# define a pin on which i attach the neopixel
+dataPin=18
+
+#create a variable for number of pixels
+pixels=16
+np=NeoPixel(Pin(dataPin, Pin.OUT),pixels)
+my_bluetooth = BLEConnection("ESP32_ReadRupin_Write")
 print("Ready! Scan for 'ESP32_Read_Write'")
-
+redValue=0
+greenValue=0
+blueValue=0
+switchPosition="false"
 # Add a timer to track when a connection actually started
 connection_start_time = 0
 
 while True:
-    my_bluetooth.check_timeout(timeout_ms=5000)
+    #my_bluetooth.check_timeout(timeout_ms=5000)
     if my_bluetooth.is_connected():       
         
         # --- ONLY DO THIS IF THE DEVICE STAYED CONNECTED ---
@@ -22,6 +32,27 @@ while True:
                 if clean_msg: 
                     raw_array = clean_msg.split(',')
                     print("Clean Array:", raw_array)
+                    #data=raw_array[0].split()
+                    
+                    if (raw_array[0]=="R"):
+                        redValue=int(raw_array[1])
+                    if (raw_array[0]=="G"):
+                        greenValue=int(raw_array[1])
+                    if (raw_array[0]=="B"):
+                        blueValue=int(raw_array[1])
+                    if (raw_array[0]=="S"):
+                        switchPosition=raw_array[1]
+                    
+#                    
+                    if(switchPosition=="true"):
+                        np.fill((redValue,greenValue,blueValue))
+                        np.write()
+                    if(switchPosition=="false"):
+                        np.fill((0,0,0))
+                        np.write()
+                        
+                    
+                    
                     
         # Existing Send Logic
         x_val = random.randint(0,800)
